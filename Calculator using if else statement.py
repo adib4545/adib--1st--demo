7d83473245a0c58f6498c9d77008e744e60c916b
Adib <114800092+adibmez@users.noreply.github.com>
2024-05-12 17:44:37 +0600

    info calc.py

---
+++ Calculator using if else statement.py
@@ -0,0 +1,26 @@
+def calculator():
+    print("Welcome to Simple Calculator!")
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+            return
+        else:
+            result = num1 / num2
+    else:
+        print("Invalid operator!")
+        return
+
+    print("Result: ", result)
+
+# Run the calculator function
+calculator()
